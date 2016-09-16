1785c5ee7b4de87dc0b905ff6b7f9669d1081742
Josh Vorick <josh.vorick@gmail.com>
2016-09-16 17:34:49 -0400

    Remove Dante game so I can actually make a perosnal site

--- Food.js
+++
@@ -1,9 +0,0 @@
-function Food(x, y) {
-    this.x = x;
-    this.y = y;
-
-    this.setImage = function(game, imgName) {
-    	this.img = game.add.sprite(this.x, this.y, imgName);
-    }
-}
-Food.prototype.type = "food";
